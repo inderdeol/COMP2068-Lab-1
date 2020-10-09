53d57465e118db281bb9e83b5b08a00ad9a2910b
Inder Deol <54917375+inderdeol@users.noreply.github.com>
2020-10-08 21:12:43 -0400

    comments added

--- helloWorld.js
+++ helloWorld.js
@@ -3,6 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// coments added to this file. 
+// server created and message sent to the dom
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
